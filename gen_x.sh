7a61bb797cf2ab28e2b87355fae2006b03bc8259
lxh <42773937+lhndo@users.noreply.github.com>
2023-05-17 20:27:53 +0000

    Update gen_x.sh

--- gen_x.sh
+++ gen_x.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+[ ! -d $HOME"/printer_data/config/RES_DATA/" ] && mkdir ~/printer_data/config/RES_DATA;
 ~/klipper/scripts/calibrate_shaper.py /tmp/resonances_x_*.csv -o ~/printer_data/config/RES_DATA/shaper_calibrate_x.png;
 Rscript ~/ResHelper/X.R;
 find '/tmp/' -name "resonances_x_*.csv" -print 2>/dev/null -exec rm {} \;
