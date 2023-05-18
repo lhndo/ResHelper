6e904814728c70d8420639fd9ea78e789d01babe
undoz <andrei@undoz.com>
2023-05-18 11:28:17 +0000

    Added damping ratio and timestamps to filenames

--- gen_x.sh
+++ gen_x.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
+echo "\nResHelper: Generating data...\n";
+name="shaper_calibrate_x";
 [ ! -d $HOME"/printer_data/config/RES_DATA/" ] && mkdir ~/printer_data/config/RES_DATA;
+
+graph generation
 ~/klipper/scripts/calibrate_shaper.py /tmp/resonances_x_*.csv -o ~/printer_data/config/RES_DATA/shaper_calibrate_x.png;
-Rscript ~/ResHelper/X.R;
-find '/tmp/' -name "resonances_x_*.csv" -print 2>/dev/null -exec rm {} \;
+
+#damping ratio
+dr=$(Rscript ~/ResHelper/X.R);
+dr=${dr#"[1] "};
+echo "Damping ratio for Y calculated:\ndamping_ratio_y: $dr\n ";
+name=$name"_dr-"$dr"_v"$(date "+%Y%m%d-%H%M")".png";
+
+
+#cleanup
+mv ~/printer_data/config/RES_DATA/shaper_calibrate_x.png ~/printer_data/config/RES_DATA/$name;
+find '/tmp/' -name "resonances_y_*.csv" -print 2>/dev/null -exec rm {} \;
