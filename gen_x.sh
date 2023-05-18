f553d4924bad1d9f34c630e773814df192e4f303
undoz <andrei@undoz.com>
2023-05-18 11:39:09 +0000

    Changing filename format

--- gen_x.sh
+++ gen_x.sh
@@ -3,14 +3,14 @@ echo "\nResHelper: Generating data...\n";
 name="shaper_calibrate_x";
 [ ! -d $HOME"/printer_data/config/RES_DATA/" ] && mkdir ~/printer_data/config/RES_DATA;
 
-graph generation
+#graph generation
 ~/klipper/scripts/calibrate_shaper.py /tmp/resonances_x_*.csv -o ~/printer_data/config/RES_DATA/shaper_calibrate_x.png;
 
 #damping ratio
 dr=$(Rscript ~/ResHelper/X.R);
 dr=${dr#"[1] "};
 echo "Damping ratio for Y calculated:\ndamping_ratio_y: $dr\n ";
-name=$name"_dr-"$dr"_v"$(date "+%Y%m%d-%H%M")".png";
+name=$name"-dr_"$dr"-v"$(date "+%Y%m%d_%H%M")".png";
 
 
 #cleanup
