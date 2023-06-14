fab0806e2bb7e84dcdd51e0bfe596c8c2dc91cfc
undoz <andrei@undoz.com>
2023-06-14 23:07:43 +0000

    fixed formatting

--- gen_y.sh
+++ gen_y.sh
@@ -10,7 +10,7 @@ name="shaper_calibrate_y";
 dr="$(Rscript ~/ResHelper/Y.R)";
 dr=${dr#"[1] "};
 echo "Damping ratio for Y calculated:\ndamping_ratio_y: $dr\n ";
-name=$name+"-dr_$dr-v$(date "+%Y%m%d_%H%M").png";
+name="$name-dr_$dr-v$(date "+%Y%m%d_%H%M").png";
 
 
 #cleanup
